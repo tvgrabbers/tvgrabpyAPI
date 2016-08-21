55295008ec947d21d536d7782fb20d3954054d83
Hika van den Hoven <hikavdh@gmail.com>
2016-08-22 00:36:34 +0200

    Added some info to tv_grab_json and an option to select only parts of the report

--- tv_grab_test_json.py
+++ tv_grab_test_json.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+# testjson.test_file accepts 3 parameters
+# file_name: The file, including a path to it, to test
+# struct_name: See the structfiles in ~/.json_struct for available names
+# report_level: Add the desired ones or substract the undesired ones from -1
+#
+# Report Levels (default is -1 or all):
+#   1: Missing required keys
+#   2: Errors on required values and on either selection
+#   4: Errors on not required keys
+#   8: Missing sugested keys without a default
+#  16: Missing sugested keys
+#  32: Missing optional keys without a default
+#  64: Missing optional keys
+# 128: Unused keys
+# 256: Unknown keys
+# 512: Report on either selection (without errors)
+
 import sys
 from tvgrabpyAPI import tv_grab_IO
 
@@ -15,6 +32,9 @@ if len(cmd) < 2:
 
 if len(cmd) == 2:
     sys.exit(testjson.test_file(cmd[1]))
+
+if len(cmd) == 3:
+    sys.exit(testjson.test_file(cmd[1], report_level = int(cmd[2])))
 
 else:
     # Adding options to test on sourceid
